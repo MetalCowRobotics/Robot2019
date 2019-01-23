4f834d3d0714fe54fb1f2cb73d402d92e20ff3d7
metalcow <35390496+metalcow@users.noreply.github.com>
2019-01-22 18:35:20 -0600

    added basse commands

---
+++ src/main/java/frc/commands/DriveStraightInches.java
@@ -0,0 +1,86 @@
+package frc.commands;
+
+import java.util.logging.Logger;
+
+import frc.lib14.MCRCommand;
+import frc.lib14.PDController;
+import frc.lib14.UtilityMethods;
+import frc.robot.RobotMap;
+import frc.systems.DriveTrain;
+
+public class DriveStraightInches extends TimedCommand implements MCRCommand {
+    private static final Logger logger = Logger.getLogger(DriveStraightInches.class.getName());
+    private boolean firstTime = true;
+    private double startTics;
+    private double targetTics;
+    protected PDController driveController;
+    private DriveTrain driveTrain = DriveTrain.getInstance();
+
+    public DriveStraightInches(double targetInches) {
+        super();
+        setTarget(targetInches);
+    }
+
+    public DriveStraightInches(double targetInches, int timeoutSeconds) {
+        super();
+        setTarget(targetInches);
+        setTargetTime(timeoutSeconds);
+    }
+
+    private void setTarget(double targetInches) {
+        targetTics = (targetInches / RobotMap.DriveWithEncoder.INCHES_PER_ROTATION)
+                * RobotMap.DriveWithEncoder.TICS_PER_ROTATION;
+        logger.info("target encoder drive:" + targetTics);
+    }
+
+    public void run() {
+        if (firstTime) {
+            firstTime = false;
+            startTimer();
+            driveTrain.resetGyro();
+            driveController = new PDController(driveTrain.getAngle());
+            startTics = driveTrain.getEncoderTics();
+        }
+        if (targetTics < ticsTravelled()) {
+            end();
+        } else {
+            driveTrain.arcadeDrive(calculateSpeed(), getCorrection());
+        }
+    }
+
+    private void end() {
+        driveTrain.stop();
+        endTimer();
+    }
+
+    private double ticsTravelled() {
+        logger.info("Drivetrain current encoder tics: " + driveTrain.getEncoderTics());
+        return driveTrain.getEncoderTics() - startTics;
+    }
+
+    private double calculateSpeed() {
+        if (targetTics - RobotMap.DriveWithEncoder.SLOW_DOWN_DISTANCE < ticsTravelled())
+            return RobotMap.DriveWithEncoder.BOTTOM_SPEED;
+        return RobotMap.DriveWithEncoder.TOP_SPEED;
+    }
+
+    private double getCorrection() {
+        logger.info("Drivetrain angle: " + driveTrain.getAngle());
+        return limitCorrection(driveController.calculateAdjustment(driveTrain.getAngle()),
+                RobotMap.DriveWithEncoder.MAX_ADJUSTMENT);
+    }
+
+    private double limitCorrection(double correction, double maxAdjustment) {
+        if (Math.abs(correction) > Math.abs(maxAdjustment))
+            return UtilityMethods.copySign(correction, maxAdjustment);
+        return correction;
+    }
+
+    public boolean isFinished() {
+        if (timerUp()) {
+            end();
+            return true;
+        }
+        return targetTics <= ticsTravelled();
+    }
+}
---
+++ src/main/java/frc/systems/Climber.java
@@ -0,0 +1,27 @@
+package frc.systems;
+
+public class Climber {
+    private static final Logger logger = Logger.getLogger(Elevator.class.getName());
+	private static final Climber instance = new Climber();
+
+	private Climber() {
+		// Singleton Pattern
+		//logger.setLevel(RobotMap.LogLevels.elevatorClass);
+	}
+
+	public static Climber getInstance() {
+		return instance;
+    }
+    
+    public void extendBackLegs() {
+    }
+
+	public void retractBackLegs() {
+	}
+
+	public void retractFrontLegs() {
+	}
+
+	public void extendFrontLegs() {
+	}
+}
