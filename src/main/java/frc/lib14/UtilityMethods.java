ff3c3690248270fd79f35df56eb3642582ba2c99
metalcow-student <46615373+metalcow-student@users.noreply.github.com>
2019-02-02 15:16:31 -0600

    Update UtilityMethods.java
    added absMax and absMin to UtilityMethods

--- src/main/java/frc/lib14/UtilityMethods.java
+++ src/main/java/frc/lib14/UtilityMethods.java
@@ -12,6 +12,7 @@ public class UtilityMethods {
 	public static boolean between(double value, double lower, double upper) {
 		return value >= lower && value <= upper;
 	}
+
 	public static double deadZoneCalculation(double input) {
 		double deadZone = 0.15;
 		if (input <= deadZone && input >= -deadZone) {
@@ -20,6 +21,7 @@ public class UtilityMethods {
 			return input;
 		}
 	}
+
 	public static double deadZoneCalculation(double input, double deadZone) {
 		if (input <= deadZone && input >= -deadZone) {
 			return 0;
@@ -31,4 +33,25 @@ public class UtilityMethods {
 	public static double round(double number, int precision) {
 		return (double) Math.round(number * precision) / precision;
 	}
+
+	public static double absMax(double input, double maxValue) {
+		double localmaxValue = Math.abs(maxValue);
+
+		if (input > 0) {
+			return Math.min(input, localmaxValue);
+		} else {
+			return Math.max(input, -localmaxValue);
+		}
+	}
+
+	public static double absMin(double input, double minValue) {
+		double localminValue = Math.abs(minValue);
+
+		if (input > 0) {
+			return Math.max(input, localminValue);
+		} else {
+			return Math.min(input, -localminValue);
+		}
+	}
+
 }
