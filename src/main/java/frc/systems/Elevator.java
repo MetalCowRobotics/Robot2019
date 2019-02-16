d6528798de447500b6898049f80dc1b7ef646d6c
MCR-coding-1 <teammetalcow+mcr1@gmail.com>
2019-02-16 12:11:56 -0600

    Changed elevator bump up
    Don't have to wait for it to bump up.

--- src/main/java/frc/systems/Elevator.java
+++ src/main/java/frc/systems/Elevator.java
@@ -23,21 +23,22 @@ public class Elevator {
 	private static final DigitalInput topLimit = new DigitalInput(RobotMap.Elevator.LIMIT_SWITCH_TOP);
 	private static final DigitalInput bottomLimit = new DigitalInput(RobotMap.Elevator.LIMIT_SWITCH_BOTTOM);
 	private static final Elevator instance = new Elevator();
-	
 	private boolean firstTime = true;
 	private double bottomTics;
 	private double topTics;
 	private PIDController holdPID;
 	public static boolean hatchMode = true;
 	private int loggingIterations = 0;
+	private int currentLevel = 1;
 
 	public boolean getHatchMode() {
 		return hatchMode;
 	}
-	
+
 	public static void setHatchMode(boolean mode) {
 		hatchMode = mode;
 	}
+
 	private Elevator() {
 		// Singleton Pattern
 		logger.setLevel(RobotMap.LogLevels.elevatorClass);
@@ -65,11 +66,11 @@ public class Elevator {
 			// setPositionTics(bottomTics); //seeing if this helps with multiple runs
 			dash.pushElevatorPIDValues();
 		}
-		if (controller.switchHeights()){
+		if (controller.switchHeights()) {
 			hatchMode = !hatchMode;
 		}
 		SmartDashboard.putBoolean("hatchmode", hatchMode);
-		getElevatorTarget(); //check for level up and level down
+		getElevatorTarget(); // check for level up and level down
 		if (0 == controller.getElevatorThrottle()) {
 			set_K_values();
 			setElevatorSpeed(holdPID.calculateAdjustment(getEncoderTics()));
@@ -132,9 +133,9 @@ public class Elevator {
 		} else {
 			if (isMovingUp(speed)) {
 				// TODO: add a variable to the robot map for max throttle
-				return UtilityMethods.copySign(speed, Math.min(Math.abs(speed), .8)); 
+				return UtilityMethods.copySign(speed, Math.min(Math.abs(speed), .8));
 			}
-			return UtilityMethods.copySign(speed, Math.min(Math.abs(speed), .8)); 
+			return UtilityMethods.copySign(speed, Math.min(Math.abs(speed), .8));
 		}
 	}
 
@@ -144,7 +145,7 @@ public class Elevator {
 
 	private boolean inUpperSafetyZone() {
 		return getEncoderTics() > (topTics - RobotMap.Elevator.SafeZone);
-	
+
 	}
 
 	public void stop() {
@@ -161,9 +162,9 @@ public class Elevator {
 
 	private boolean isElevatorAtBottom() {
 		if (!bottomLimit.get()) {
-			//reset bottom and top measures
-			// bottomTics = getEncoderTics();
-			// topTics = bottomTics + inchesToTics(RobotMap.Elevator.ELEVATOR_MAX_EXTEND);
+			// reset bottom and top measures
+			bottomTics = getEncoderTics();
+			topTics = bottomTics + inchesToTics(RobotMap.Elevator.ELEVATOR_MAX_EXTEND);
 		}
 		return !bottomLimit.get(); // for some reason this is inverted in hardware, correcting here in software
 	}
@@ -176,7 +177,8 @@ public class Elevator {
 		loggingIterations++;
 		if (20 < loggingIterations) {
 			logger.info("Elevator throttle:" + controller.getElevatorThrottle());
-			logger.info("Elevator Up limit: " + this.isElevatorAtTop() + " Elevator Down limit: " + this.isElevatorAtBottom());
+			logger.info("Elevator Up limit: " + this.isElevatorAtTop() + " Elevator Down limit: "
+					+ this.isElevatorAtBottom());
 			logger.info("Elevator bottom tics:" + bottomTics + "   Elevator top tics:" + topTics);
 			logger.info("Elevator Speed:" + ELEVATOR_MOTOR.get());
 			logger.info("Elevator encoder tics:" + getEncoderTics());
@@ -193,28 +195,57 @@ public class Elevator {
 	private void getElevatorTarget() {
 		// need to add a mode to switch between cargo and hatches in execute
 		if (hatchMode) {
-			determineLevel(RobotMap.Elevator.HATCH_LEVEL_1,RobotMap.Elevator.HATCH_LEVEL_2,RobotMap.Elevator.HATCH_LEVEL_3);
+			determineLevel(RobotMap.Elevator.HATCH_LEVEL_1, RobotMap.Elevator.HATCH_LEVEL_2,
+					RobotMap.Elevator.HATCH_LEVEL_3);
 		} else {
-			determineLevel(RobotMap.Elevator.BALL_HEIGHT_1, RobotMap.Elevator.BALL_HEIGHT_2, RobotMap.Elevator.BALL_HEIGHT_3);
-		}	
+			determineLevel(RobotMap.Elevator.BALL_HEIGHT_1, RobotMap.Elevator.BALL_HEIGHT_2,
+					RobotMap.Elevator.BALL_HEIGHT_3);
+		}
 	}
 
+	// private void determineLevel(double level1, double level2, double level3) {
+	// double fudgeFactor = 50; // if the PID does not get it to height it will
+	// always be lower and never go to the else
+	// logger.info("current distance: " + (getEncoderTics() - bottomTics) + " <> ");
+	// if (controller.upLevel()) {
+	// if ((getEncoderTics() - bottomTics) < (level2 - fudgeFactor)) {
+	// setPositionTics(level2 + bottomTics);
+	// } else {
+	// setPositionTics(level3 + bottomTics);
+	// }
+	// }
+	// if (controller.downLevel()) {
+	// if ((getEncoderTics() - bottomTics) > level2) {
+	// setPositionTics(level2 + bottomTics);
+	// } else {
+	// setPositionTics(level1 + bottomTics);
+	// }
+	// }
+	// }
+	// handling level changes/sets level
+	// level1-3 - Tic count for each level
 	private void determineLevel(double level1, double level2, double level3) {
-		double fudgeFactor = 50; // if the PID does not get it to height it will always be lower and never go to the else
-		logger.info("current distance: " + (getEncoderTics() - bottomTics) + " <> ");
 		if (controller.upLevel()) {
-			if ((getEncoderTics() - bottomTics) < (level2 - fudgeFactor)) {
-				setPositionTics(level2 + bottomTics);
-			} else {
-				setPositionTics(level3 + bottomTics);
+			if (currentLevel < 3) {
+				currentLevel++;
 			}
+			setLevel(level1, level2, level3);
 		}
 		if (controller.downLevel()) {
-			if ((getEncoderTics() - bottomTics) > level2) {
-				setPositionTics(level2 + bottomTics);
-			} else {
-				setPositionTics(level1 + bottomTics);
+			if (currentLevel > 1) {
+				currentLevel--;
 			}
+			setLevel(level1, level2, level3);
+		}
+	}
+
+	private void setLevel(double level1, double level2, double level3) {
+		if (currentLevel == 1) {
+			setPositionTics(level1 + bottomTics);
+		} else if (currentLevel == 2) {
+			setPositionTics(level2 + bottomTics);
+		} else if (currentLevel == 3) {
+			setPositionTics(level3 + bottomTics);
 		}
 	}
 }
